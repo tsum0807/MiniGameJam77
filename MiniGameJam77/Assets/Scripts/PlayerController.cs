fa744bdfc138048670091d9d61907f09b371160c
tsum0807 <44571776+tsum0807@users.noreply.github.com>
2021-04-02 13:22:18 +0700

    Initial player movement

---
+++ MiniGameJam77/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 2f;
+
+    private Rigidbody2D rigidBody;
+
+    void Start(){
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        HandleInput();
+    }
+
+    private void HandleInput(){
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        
+        // Pass horizontal and vertical directions
+        Move(h, v);
+    }
+
+    private void Move(float h, float v){
+        //
+        Vector2 velocity = new Vector2();
+        velocity.x = speed * h * Time.deltaTime;
+        velocity.y = speed * v * Time.deltaTime;
+
+        rigidBody.velocity = velocity;
+        // gameObject.transform.position = new Vector2 (transform.position.x + (h * speed), transform.position.y + (v * speed));
+    }
+}
